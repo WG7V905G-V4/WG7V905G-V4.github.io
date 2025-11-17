3386df011a67e934ccc4e9a74abca166ead24fd8
WG7V905G-V4 <kostya.nadolin@gmail.com>
2025-11-17 21:12:48 +0200

    first commit

--- funk tests.js
+++
@@ -1,7 +0,0 @@
-function getRandomBallsCount(level) {
-    let max = Math.floor(level/3)+4;
-    console.log(max);
-    return Math.floor(Math.random() * (max - 3)) + 2;
-}
-
-console.log(getRandomBallsCount())

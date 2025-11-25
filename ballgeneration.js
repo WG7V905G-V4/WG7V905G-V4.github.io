885567b5743952ca267a4056c0e89743bc8c0802
WG7V905G-V4 <kostya.nadolin@gmail.com>
2025-11-25 19:30:41 +0200

    полный рефакторинг незавершенная замена метода анимации

--- ballgeneration.js
+++ ballgeneration.js
@@ -1,31 +1,37 @@
+function getRandomBallsCount(level) {
+    const absGameMax = 10;
+    const steepness = 0.7;
+    const levelBreakpoint = 10;
+    const numSlope =1+Math.floor( absGameMax * (1/ (1+Math.E**(-steepness*(level - levelBreakpoint)))))
+    const upper = numSlope>3;
 
-export function getRandomBallsCount(level) {
-    const min = 3;
-    const max = Math.max(min + 1, Math.floor(level/3) + 5);
+    let max =  numSlope;
+    let min = 3;
+    if (!upper) {
+        max = 3;
+        min = numSlope;
+    }
 
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-export function genColor() {
-    let hue = Math.floor(Math.random() * 361); // 0 ... 360
-    const saturation = Math.floor(Math.random() * 31 + 70); // 60 ... 100
+function genColor() {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 31 + 70);
     return `hsl(${hue}, ${saturation}%, 50%)`;
 }
 
 
-export function getRandomSize(centerRadius) {
-    return Math.random() * 20 + centerRadius * 0.7;
-}
+
 
 export class Ball {
-    constructor(radius, color, top, left, toTop, toLeft, blur) {
+    constructor(radius, color, top, left, distanceX, distanceY, blur) {
         this.radius = radius;
         this.color = color;
         this.top = top;
         this.left = left;
-        this.toTop = toTop;
-        this.toLeft = toLeft;
+        this.distanceX = distanceX;
+        this.distanceY = distanceY;
         this.blur = blur;
     }
 }
@@ -34,32 +40,43 @@ export class Ball {
 export function* ballGenerator(level, centerRect) {
     const numOfBalls = getRandomBallsCount(level);
 
+
     const grayBallCenterX = centerRect.left + (centerRect.width/2) ;
     const grayBallCenterY = centerRect.top + (centerRect.height/2)  + window.scrollY;
-
     const centerBallDiameter = centerRect.width * 0.8;
-    const biasAngle = Math.random() * 100;
-    for (let i = 0; i < numOfBalls; i++) {
 
+
+    const biasAngle = Math.random() * 100;
+
+
+    for (let i = 0; i < numOfBalls; i++) {
         const angle = ((2 * Math.PI) * i / numOfBalls) + (Math.random()*3 - 0.5) * 0.05 + biasAngle;
-        const radius = getRandomSize(centerBallDiameter);
-        const distance = 100 + Math.random() * 40;
-        const distanceX = Math.cos(angle) * distance;
-        const distanceY = Math.sin(angle) * distance;
+        const radius = centerBallDiameter+Math.min(Math.max(Math.random(), 1/(level+1)), 0.7)
+
+
         const color = genColor();
+
 
         const top = (grayBallCenterY - radius / 2) + "px";
         const left = (grayBallCenterX - radius / 2) + "px";
-        const toTop = (grayBallCenterY + distanceY - radius / 2) + "px";
-        const toLeft = (grayBallCenterX + distanceX - radius / 2) + "px";
+
+
+        const distance = 100 + Math.random() * 80;
+        const distanceX = Math.cos(angle) * distance;
+        const distanceY = Math.sin(angle) * distance;
+
+
+
+
 
         yield new Ball(
             radius + "px",
             color,
             top,
             left,
-            toTop,
-            toLeft
+            distanceX,
+            distanceY
         );
     }
 }
+
--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 import { ballGenerator } from './ballgeneration.js';
-import { sendData, getData, getUserId } from './serverReq.js';
+import { btnState, btnHandler } from './btnStates.js';
 
 const userCountSpan = document.getElementById("user-count");
 const centerBall = document.getElementById("center-ball");
-const mainBtn = document.getElementById("main-btn");
-const decBtn = document.getElementById("decrement");
-const incBtn = document.getElementById("increment");
 const scoreSpan = document.getElementById("score");
 const topScoreSpan = document.getElementById("topScore");
+const decBtn = document.getElementById("decrement");
+const incBtn = document.getElementById("increment");
+const mainBtn = document.getElementById("main-btn");
+
+
 
 let level = 0;
 let gameState = "start";
 let showCount = 0;
-let userId = getUserId();
-let topScore = await getData(userId);
-
-console.log('User ID:', userId);
-console.log('Top Score:', topScore);
-
-topScoreSpan.textContent = topScore;
+let topScore = localStorage.getItem("topScore")||0;
 
 function padding(n) {
     return String(n).padStart(3, '0');
@@ -29,115 +25,113 @@ function setUserCount(n) {
     userCountSpan.textContent = padding(n);
 }
 
-function setMainBtnHandler(handler) {
-    mainBtn.onclick = null;
-    mainBtn.onclick = handler;
-}
+function start(){
+    btnState(
+        mainBtn,
+        true
+    )
+    btnState(
+        [incBtn, decBtn],
+        false
+    )
 
-function setMainBtnState({ text, disabled, bg, color, filter }) {
-    mainBtn.textContent = text;
-    mainBtn.classList.toggle("disabled", disabled);
-    mainBtn.style.pointerEvents = disabled ? "none" : "auto";
-    if (bg) mainBtn.style.background = bg;
-    if (color) mainBtn.style.color = color;
-    if (filter) mainBtn.style.filter = filter;
-}
-
-incBtn.onclick = () => {
-    if (gameState === "input") setUserCount(Math.min(showCount + 1, 999));
-};
-
-decBtn.onclick = () => {
-    if (gameState === "input") setUserCount(Math.max(showCount - 1, 0));
-};
-
-function startGame() {
-    if (gameState !== "start") return;
-    countMessage();
-}
-
-setMainBtnHandler(startGame);
-
-function countMessage() {
-    gameState = "anim";
-    setMainBtnState({
-        text: "count",
-        disabled: true,
-        bg: "#333",
-        color: "#dadada",
-        filter: "blur(0px)"
+    btnHandler(mainBtn, () => {
+        if (gameState === "start") count();
     });
+    btnHandler(incBtn, () => {
+        if (gameState === 'input') setUserCount(Math.min(showCount + 1, 999));
+    });
+    btnHandler(decBtn, () => {
+        if (gameState === "input") setUserCount(Math.max(showCount - 1, 0));
+    });
+}
+
+
+function count() {
+    gameState = "anim";
+    btnState(
+        mainBtn,
+        false,
+        "count")
     animation();
+}
+function* htmlBall(ballObj, time){
+    let ballDiv = document.createElement("div");
+    ballDiv.className = "ball";
+    ballDiv.style.width = ballDiv.style.height = ballObj.radius;
+    ballDiv.style.background = ballObj.color;
+    ballDiv.style.top = ballObj.top;
+    ballDiv.style.left = ballObj.left;
+    ballDiv.style.opacity = "1";
+    ballDiv.style.transition = "transform 0.s";
+    document.body.appendChild(ballDiv);
+    yield ballDiv;
 }
 
 function animation() {
-    gameState = "anim";
+    const time = Math;
+
     const ballsCLass = [...ballGenerator(level, centerBall.getBoundingClientRect())];
     const ballsDiv = [];
-
     for (const ballObj of ballsCLass) {
-        if (ballObj) {
-            let ballDiv = document.createElement("div");
-            ballDiv.className = "ball";
-            ballDiv.style.width = ballDiv.style.height = ballObj.radius;
-            ballDiv.style.background = ballObj.color;
-            ballDiv.style.top = ballObj.top;
-            ballDiv.style.left = ballObj.left;
-            ballDiv.style.opacity = "1";
-            document.body.appendChild(ballDiv);
-            ballsDiv.push(ballDiv);
-        }
+        if (ballObj) ballsDiv.push(htmlBall(ballObj).next().value);
     }
+    btnState(
+        [mainBtn, incBtn, decBtn],
+        false
+        )
+
+    requestAnimationFrame(()=>{
+        requestAnimationFrame(()=>{
+            ballsDiv.forEach((ballDiv, i) => {
+                ballDiv.style.transform = `translate(${ballsCLass[i].distanceX}px, ${ballsCLass[i].distanceY}px)`;
+            });
+        })
+    })
 
     setTimeout(() => {
-        ballsDiv.forEach((ballDiv, i) => {
-            ballDiv.style.transition =
-                "top 0.7s cubic-bezier(.4,0,.2,1), left 0.7s cubic-bezier(.4,0,.2,1), opacity 380ms cubic-bezier(.4,0,.2,1)";
-            ballDiv.style.opacity = "1";
-            ballDiv.style.top = ballsCLass[i].toTop;
-            ballDiv.style.left = ballsCLass[i].toLeft;
-        });
+
     }, 100);
 
     setTimeout(() => {
-        ballsDiv.forEach((ballDiv, i) => {
-            ballDiv.style.opacity = "1";
-            ballDiv.style.top = ballsCLass[i].top;
-            ballDiv.style.left = ballsCLass[i].left;
+        ballsDiv.forEach((ballDiv) => {
+            ballDiv.style.transform = "translate(0, 0)";
         });
-        setTimeout(() => {
+        setTimeout(()=>{
             ballsDiv.forEach(ball => ball.remove());
-            inputStage(ballsDiv.length);
         }, 900);
-    }, 500 + Math.max(0, 650 - level * 60));
+    }, 100 + Math.max(0, 650 - level * 60));
+
+
+    inputStage(ballsDiv.length);
 }
+
 
 function inputStage(rightAnswer) {
     gameState = "input";
     setUserCount(0);
-    setMainBtnState({
-        text: "count",
-        disabled: true,
-        bg: "#333",
-        color: "#dadada",
-        filter: "blur(0px)"
-    });
 
+
+    btnState(
+        [incBtn, decBtn],
+        true
+    )
     setTimeout(() => {
-        setMainBtnState({
-            text: "done",
-            disabled: false,
-            bg: "#dadada",
-            color: "#333",
-            filter: "blur(0px)"
-        });
-        setMainBtnHandler(() => {
+        btnState(
+            mainBtn,
+            true,
+            "done"
+        );
+
+        btnHandler(mainBtn, () =>
+        {
+            btnState(
+                [incBtn, decBtn],
+                false
+            )
             if (gameState !== "input") return;
-            if (rightAnswer === showCount) {
-                rightAnswerAnim();
-            } else {
-                wrongAnswer();
-            }
+            if (rightAnswer === showCount) rightAnswerAnim();
+            else wrongAnswerAnim();
         });
     }, 650);
 }
@@ -149,7 +143,6 @@ function rightAnswerAnim() {
     if (level > topScore) {
         topScore = level;
         topScoreSpan.textContent = topScore;
-        sendData(userId, topScore);
     }
 
     centerBall.style.background = "#00aa2a";
@@ -160,8 +153,8 @@ function rightAnswerAnim() {
     }, 1200);
 }
 
-function wrongAnswer() {
-    centerBall.style.background = "#ef1e48";
+function wrongAnswerAnim() {
+    centerBall.style.background = "#df0d38";
     centerBall.style.filter = "blur(22px)";
 
     setTimeout(() => {
@@ -169,31 +162,24 @@ function wrongAnswer() {
     }, 1100);
 }
 
-async function resetGame(fail = false) {
-    centerBall.style.background = "#ffffff";
+function resetGame(fail = false) {
+    centerBall.style.background = "#dadada";
     centerBall.style.filter = "blur(0px)";
     setUserCount(0);
     gameState = fail ? "start" : "anim";
 
     if (fail) {
-        if (level > topScore) {
-            await sendData(userId, level);
-            topScore = level;
-            topScoreSpan.textContent = topScore;
-        }
-
+        localStorage.setItem("topScore", topScore);
         level = 0;
-        scoreSpan.textContent = "0";
-        setMainBtnHandler(startGame);
-        setMainBtnState({
-            text: "start game",
-            disabled: false,
-            bg: "#dadada",
-            color: "#23232a",
-            filter: "blur(0px)"
-        });
+        scoreSpan.textContent = level;
+        btnHandler(mainBtn, start);
+        btnState(
+            mainBtn,
+            true,
+            "start game",
+        );
     } else {
-        countMessage();
+        count();
     }
 }
 
@@ -201,12 +187,5 @@ window.onload = async () => {
     setUserCount(0);
     scoreSpan.textContent = level;
     topScoreSpan.textContent = topScore;
-    setMainBtnState({
-        text: "start game",
-        disabled: false,
-        bg: "#dadada",
-        color: "#23232a",
-        filter: "blur(0px)"
-    });
-    setMainBtnHandler(startGame);
+    start()
 };
